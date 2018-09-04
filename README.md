01afa3ead3ce96176641e4826ae5ca4087872b08
WustLCQ <wustlcq@foxmail.com>
2018-09-04 17:06:04 +0800

    fix 修复末尾省略号会遮住一部分文字的bug

--- README.md
+++ README.md
@@ -10,19 +10,19 @@ npm install --save vue-multline
 
 ```html
 <vue-multline
+    :text="text"
     :line="3"
     :autoCollapse="true"
-    endChars="..."
+    :endChars="..."
     :showExpandBtn="true"
->
-    {{content}}
-</vue-multline>
+/>
 ```
 
 # 参数
 
 | 名称 | 说明 | 参数类型| 默认值 |
 | - | :-: | :-: | :-: |
+| text | 文本内容 | String | - |
 | line | 展示的行数 | Number | 3 |
 | autoCollapse | 是或否默认收起 | Boolean | true |
 | endChars | 末尾符号 | String | ... |
@@ -30,8 +30,3 @@ npm install --save vue-multline
 | expandBtnText | 展开按钮文字 | String | 展开 |
 | showCollapseBtn | 是否显示收起按钮 | Boolean | true |
 | collapseBtnText | 收起按钮文字 | String | 收起 |
-| leftSpace | 左边补白 | String,css长度值，如5px，1rem | "" |
-| rightSpace | 左边补白 | String,css长度值，如5px，1rem | "" |
-| bgColor | 末尾按钮背景色 | String,css颜色值，如#fff,rgba(0,0,0) | #fff |
-
-由于末位的...以及展开收起按钮采用的是绝对定位方式，有时候会出现...遮挡住一部分文字以及按钮与上一行文字不对齐的情况，此时需要通过*leftSpace*与*rightSpace*修正
--- src/index.vue
+++ src/index.vue
@@ -1,32 +1,25 @@
 <template>
-    <p
-        ref="text"
-        class='m-multline'
-        :style="styleContainer"
-    >
-        <slot></slot>
+    <div ref="box" class='m-multline' :style="styleContainer">
+        <p ref="text" class="text">{{content}}</p>
         <span
-            v-if="!isLessLine"
+            v-if="hasDeleteWords && collapse && showExpandBtn"
             class="end"
-            :style="styleEnd"
-        >
-            <span v-if="collapse" class="end-chars">{{endChars}}</span>
-            <span
-                v-if="collapse && showExpandBtn"
-                class="end-btn"
-                @click="handleExpand"
-            >{{expandBtnText}}</span>
-            <span
-                v-else-if="!collapse && showCollapseBtn"
-                class="end-btn"
-                @click="handleCollapse"
-            >{{collapseBtnText}}</span>
-        </span>
-    </p>
+            @click="handleExpand"
+        >{{expandBtnText}}</span>
+        <span
+            v-else-if="hasDeleteWords &&!collapse && showCollapseBtn"
+            class="end"
+            @click="handleCollapse"
+        >{{collapseBtnText}}</span>
+    </div>
 </template>
 <script>
 export default {
     props: {
+        text: {
+            type: String,
+            required: true
+        },
         line: {
             type: Number,
             default: 3
@@ -54,38 +47,19 @@ export default {
         collapseBtnText: {
             type: String,
             default: '收起'
-        },
-        leftSpace: {
-            type: String,
-            default: ''
-        },
-        rightSpace: {
-            type: String,
-            default: ''
-        },
-        bgColor: {
-            type: String,
-            default: '#fff'
         }
-
     },
     data () {
         return {
             collapse: true,
-            isLessLine: false,
+            hasDeleteWords: false,
+            content: this.text
         }
     },
     computed: {
         styleContainer () {
             return {
                 overflow: this.collapse ? 'hidden' : ''
-            }
-        },
-        styleEnd () {
-            return {
-                background: this.bgColor,
-                paddingLeft: this.leftSpace,
-                paddingRight: this.rightSpace
             }
         }
     },
@@ -95,50 +69,64 @@ export default {
     mounted () {
         this.init();
     },
-    updated () {
-        // 有可能在父组件中更改了样式，所以updated时需要重新执行一遍
-        this.init();
-    },
     methods: {
         init () {
             const textEle = this.$refs.text;
-            const textStyle = getComputedStyle(textEle);
+            let textStyle = getComputedStyle(textEle);
             const fontSize = Number.parseFloat(textStyle.fontSize.replace('px', ''), 10);
             // 各个浏览器对line-height默认值normal实现不同，一般在字体的1.0到1.2倍之间，这里采用1.2倍
             const lineHeight = textStyle.lineHeight === 'normal' ? fontSize * 1.2
                 : Number.parseFloat(textStyle.lineHeight.replace('px', ''), 10);
 
             textEle.style.height = '';
-            const autoHeight = Number.parseFloat(textStyle.height.replace('px', ''), 10);
+            let autoHeight = Number.parseFloat(textStyle.height.replace('px', ''), 10);
             // 若文字内容不足设定行数，直接不展示末尾按钮
             if (autoHeight < lineHeight * this.line) {
-                this.isLessLine = true;
                 return;
             }
+            this.hasDeleteWords = true;
+            const deleteEndWords = () => {
+                this.content = this.content.substr(0, this.content.length - 1);
+                this.$nextTick(() => {
+                    textStyle = getComputedStyle(textEle);
+                    textEle.style.height = '';
+                    autoHeight = Number.parseFloat(textStyle.height.replace('px', ''), 10);
+                    if (autoHeight > lineHeight * this.line) {
+                        deleteEndWords();
+                    } else {
+                        // 最后一行末尾少2个字符
+                        this.content = this.content.substr(0, this.content.length - 3) + this.endChars;
+                    }
+                });
+            }
+            deleteEndWords();
             // 在收起状态才需要设置height
             if (this.collapse) {
                 textEle.style.height = lineHeight * this.line + 'px';
-            }
-
-            // 将内联样式中的position清除，然后当前的position是不是static，如果是，则设置为relative
-            textEle.style.position = '';
-            if (textStyle.position === 'static') {
-                textEle.style.position = 'relative';
             }
         },
         // 展开
         handleExpand () {
             this.collapse = false;
             this.height = '';
+            this.content = this.text;
         },
         // 收起
         handleCollapse () {
             this.collapse = true;
+            this.init();
         }
     }
 }
 </script>
 <style>
+.m-multline {
+    position: relative;
+}
+.m-multline .text {
+    margin: 0;
+    padding: 0;
+}
 .m-multline .end {
     position: absolute;
     bottom: 0;
@@ -146,4 +134,3 @@ export default {
     color: #576B95;
 }
 </style>
-
